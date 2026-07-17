d42545afc1deaf6f4fe21acb7abd8c422fecfb22
MOJOEA <DevineTYpor@gmail.com>
2026-07-17 15:29:20 +0700

    new

--- 02-UI-and-Styling/angular-ui/src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js'; // ◄ เพิ่มบรรทัดนี้ไว้ที่แถวแรกสุดของไฟล์!
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.ts'; // หรือชื่อไฟล์หลักของคุณ
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
